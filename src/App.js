b4ca27f98f26241e6251d42a7bc2284f3553e885
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-18 20:32:02 -0300

    Set routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Cadastro from './components/Cadastro.js'
+import Home from './components/Home.js'
+import Main from './components/Main.js'
+import NovaEntrada from './components/NovaEntrada.js'
+import NovaSaida from './components/NovaSaida.js'
+
 export default function App(props){
-    return(<>
-        <BrowserRouter>
-            <Routes>
-                
-            </Routes>
-        </BrowserRouter>
-    </>)
+	return(<>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main/>}/>
+				<Route path="/cadastro" element={<Cadastro/>}/>
+				<Route path="/home" element={<Home/>}/>
+				<Route path="nova-entrada" element={<NovaEntrada/>}/>
+				<Route path="nova-saida" element={<NovaSaida/>}/>
+			</Routes>
+		</BrowserRouter>
+	</>)
 }
