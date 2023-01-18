506e89a4d4f06c67f757520d887503a11f340f00
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-18 19:55:31 -0300

    Created project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+export default function App(props){
+    return(<>
+        <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+        </BrowserRouter>
+    </>)
+}
